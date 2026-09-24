262924a7c02b25afaa45869a6829d14270b82f56
Delean Mafra <170483770+Delean-Mafra@users.noreply.github.com>
2026-09-23 21:09:05 -0300

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/kambam_game/',
+  base: './',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
